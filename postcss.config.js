1a6d7550ea300f5714346a02dd01a434fc4cdc97
Jareth Rader <jarethrader@gmail.com>
2021-03-01 02:48:04 -0700

    updated tailwindcss and dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,20 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
-const cssnano = require('cssnano');
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
 
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    process.env.NODE_ENV === 'production' && cssnano({ preset: 'default' }),
-    process.env.NODE_ENV === 'production' &&
+    require("tailwindcss"),
+    require("autoprefixer"),
+    process.env.NODE_ENV === "production" && cssnano({ preset: "default" }),
+    process.env.NODE_ENV === "production" &&
       purgecss({
         content: [
-          './src/**/*.ts',
-          './src/**/*.tsx',
-          './src/**/*.js',
-          './src/**/*.jsx',
+          "./src/**/*.ts",
+          "./src/**/*.tsx",
+          "./src/**/*.js",
+          "./src/**/*.jsx",
         ],
         defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-        whitelistPatterns: [/page/, /slide/, /fade/],
-        whitelistPatternsChildren: [/page/, /slide/, /fade/],
       }),
   ],
 };
